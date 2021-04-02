e71df2071bec0593e9d63451f9205ee775ac2697
Iago <iagobrazmendes@gmail.com>
2021-04-02 10:32:47 -0300

    connect to database; create user model

---
+++ src/models/User.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+export type UserType = mongoose.Document & 
+{
+	id: number
+	processedMessages: number[]
+	stage: number
+	startedAt: string // Date
+}
+
+const UserSchema = new mongoose.Schema(
+	{
+		id: {type: Number, required: true},
+		processedMessages: [{type: Number, required: true}],
+		stage: {type: Number, required: true},
+		startedAt: {type: Date, default: Date.now(), expires: 24 * 3600}
+	})
+
+export default mongoose.model<UserType>('User', UserSchema)
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'express-async-errors'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 import routes from './routes'
 import errorHandler from './errors/handler'
@@ -12,6 +13,21 @@ dotenv.config()
 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://test:${process.env.DB_PWD}@main.crou6.mongodb.net/main?retryWrites=true&w=majority`
+mongoose.connect(
+	uri,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	}
+)
+mongoose.connection
+	.once('open', () => console.log('database connected'))
+	.on('error', error => console.log('[database connection error]:', error))
+
 
 app.use(routes)
 
