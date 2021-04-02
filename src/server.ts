ae669b3c987e593f3967fdb0d22349abac6671df
Iago <iagobrazmendes@gmail.com>
2021-04-02 09:59:31 -0300

    get updates and send text

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 
 import routes from './routes'
 import errorHandler from './errors/handler'
+import api from './services/api'
 
 const app = express()
 dotenv.config()
@@ -13,6 +14,40 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
+
+let offset: number | undefined = undefined
+
+setInterval(() =>
+{
+	const data = offset
+		? {offset}
+		: {}
+
+	api.post('getUpdates', data)
+		.then(res =>
+		{
+			const update = res.data.result[0]
+			if (update)
+			{
+				offset = update.update_id + 1
+
+				const params =
+				{
+					chat_id: update.message.chat.id,
+					text:
+					'🎉 Olá! Eu sou um bot! 🎉\n' +
+					'Você me mandou a seguinte mensagem:\n\n' +
+					update.message.text
+				}
+
+				api.post('sendMessage', params)
+			}
+		})
+		.catch(error =>
+		{
+			console.error('[error]', error.response.data)
+		})
+}, 2*1000)
 
 app.use(errorHandler)
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const token = process.env.TELEGRAM_TOKEN
 
 const api = axios.create(
